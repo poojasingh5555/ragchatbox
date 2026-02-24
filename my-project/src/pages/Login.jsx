c79d096ea5d15be001160b0817ea5a04d3abda80
poojasingh5555 <159644681+poojasingh5555@users.noreply.github.com>
2026-02-24 13:50:56 +0530

    Update Login.jsx

--- my-project/src/pages/Login.jsx
+++ my-project/src/pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     setError("");
 
     try {
-      const res = await axios.post("http://localhost:5000/api/login", {
+      const res = await axios.post("https://ragchatbox-b5pe.onrender.com", {
         email,
         password,
       });
